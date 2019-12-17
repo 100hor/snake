5ddabd02865d6d061c9793886e72e2cdc0d670c8
Yehor Stolba <yehor.stolba@nure.ua>
2019-12-17 14:24:40 +0200

    changed move method

--- src/main/java/com/snake/model/Snake.java
+++ src/main/java/com/snake/model/Snake.java
@@ -2,7 +2,6 @@ package com.snake.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import static com.snake.model.MoveEvent.MoveEventType.*;
